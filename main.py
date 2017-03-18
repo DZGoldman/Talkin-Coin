987c532490011078e804bbc42c5aac321a8931eb
Daniel Goldman <dzgoldman@wesleyan.edu>
2017-03-18 16:52:41 -0400

    hosting not working

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from slack_api import connect
-connect()
+from flask import Flask
+import os
+app = Flask(__name__)
+print('name', __name__)
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    connect()

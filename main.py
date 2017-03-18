f54cf6ffe7afe371c7181bc2149d0c934fd00431
Daniel Goldman <dzgoldman@wesleyan.edu>
2017-03-18 17:27:02 -0400

    succesfully hosting with flask

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from slack_api import connect
 from flask import Flask
 import os
 app = Flask(__name__)
-print('name', __name__)
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+@app.route('/')
+def start():
     connect()
+    return "app running"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=port)

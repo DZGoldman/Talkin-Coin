dcc0b1e6c0933c443953e07cde6c67abe53b8676
Daniel Goldman <dzgoldman@wesleyan.edu>
2017-03-18 22:28:30 -0400

    switched to web hooks

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from slack_api import connect
-from flask import Flask
-import os
+from flask import Flask, request
+import os, json
+from slacker import Slacker
+from coin_api import *
+
+webhook_token = os.environ['webhook_token']
+coin_bot_token = os.environ['coin_bot_token']
+goldman_slack_id = os.environ['goldman_slack_id']
+
+slack = Slacker(coin_bot_token)
+
 app = Flask(__name__)
-@app.route('/')
-def start():
-    connect()
-    return "app running"
+
+@app.route('/', methods=['Post'])
+def recieve():
+    print(request.data)
+    data = json.loads(request.data)
+    if data.token == webhook_token:
+        text, user_id, channel_id = data['text'], data['user_id'], data['channel_id']
+        user_tag = user_tag = "<@%s>" %(user)
+        slack.chat.post_message(channel_id, user_tag + ' ' + get_data( get_stats(text)))
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 3000))
+    slack.chat.post_message(goldman_slack_id, 'starting server connecting')
     app.run(host='0.0.0.0', port=port)

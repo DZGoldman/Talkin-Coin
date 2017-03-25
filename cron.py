c3b5f37bc1f63ea11bbba80ac6c8f613942d83a1
Daniel Goldman <dzgoldman@wesleyan.edu>
2017-03-24 23:13:26 -0400

    asdf

--- cron.py
+++ cron.py
@@ -2,6 +2,6 @@ from slack_api import *
 def send_eth_price (data):
     price  = data['price']['usd']
     print('cron: eth price', price)
-    if (price < 40) or  (price > 50):
+    if (price < 40) or  (price > 55):
         slack.chat.post_message(goldman_slack_id, str(price))
 get_single_coin('eth', send_eth_price)

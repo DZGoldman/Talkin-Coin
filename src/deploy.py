e0201b267a508495e4f6e0a26f21c1a99d214cac
Daniel Goldman <dzgoldman@wesleyan.edu>
2017-05-30 20:12:42 -0400

    removed deploy script (it's a githook now)

--- src/deploy.py
+++
@@ -1,8 +0,0 @@
-import os
-# from test import CoinApiTests
-
-exit_code = os.system("python src/test.py")
-if (exit_code == 0):
-    os.system('git push --force heroku master')
-else:
-    print('Tests failed, deploy cancelled.')
